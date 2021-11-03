4dc5336219ec3610eb6a15e20046536f3c6b0ef7
Kate Sv <radistkatya@gmail.com>
2021-11-03 13:20:54 -0700

    added detail page

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const {client, syncAndSeed} =require('./db')
+const {client, syncAndSeed} = require('./db');
 const express = require('express');
 const path = require('path')
 
 const app =express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
 app.get('/', async(req, res, next)=> {
   try{
     const response = await client.query('SELECT * FROM "Brand";');
@@ -21,7 +22,7 @@ app.get('/', async(req, res, next)=> {
         ${
           brands.map (brand => `
           <li>
-            <a href="/barnds/${brand.id}">
+            <a href="/brands/${brand.id}">
             ${brand.name}
             </a>
           </li>
@@ -35,8 +36,50 @@ app.get('/', async(req, res, next)=> {
   catch(ex){
     next(ex)
   }
-
 });
+
+app.get('/brands/:id', async(req, res, next)=> {
+  try{
+    // let response = await client.query('SELECT * FROM "Brand" WHERE id=$1;', [req.params.id]);
+    // const brand = response.rows[0];
+    // response = await client.query('SELECT * FROM "Sneaker" WHERE brand_id=$1;', [req.params.id]);
+    // const sneakers = response.rows
+
+    const promises = [
+      client.query('SELECT * FROM "Brand" WHERE id=$1;', [req.params.id]),
+      client.query('SELECT * FROM "Sneaker" WHERE brand_id=$1;', [req.params.id])
+    ];
+    const [barndsResponse, sneakersResponse] = await Promise.all(promises);
+    const brand =barndsResponse.rows[0];
+    const sneakers = sneakersResponse.rows;
+    res.send(`
+    <html>
+    <head>
+      <link rel='stylesheet' href='/assets/style.css'/> 
+    </head>
+    <body>
+      <h1>Sneaker World</h1>
+      <h2><a href='/'>Brands</a> (${brand.name})</h2>
+      <ul>
+      ${
+        sneakers.map (sneaker => `
+        <li>
+          <a href="/brands/${brand.id}">
+          ${sneaker.name}
+          </a>
+        </li>
+        `).join('')
+      }
+      </ul>
+    </body>
+    </html>
+    `);
+  }
+  catch(ex){
+    next(ex)
+  }
+});
+
 
 const port = process.env.PORT || 3000;
 
